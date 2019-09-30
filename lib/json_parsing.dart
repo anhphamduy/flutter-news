d31757ad5eeff5dc40168ef4a510ab24c7236702
Anh Pham <anh.pham@lodlaw.com>
2019-09-30 23:21:35 +1000

    Update properties

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,35 +1,29 @@
 import 'dart:convert' as json;
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 
 part 'json_parsing.g.dart';
 
 abstract class Article implements Built<Article, ArticleBuilder> {
   int get id;
+  bool get deleted;
+  String get type;
+  String get by;
+  int get time;
+  String get text;
+  bool get dead;
+  int get parent;
+  int get poll;
+  BuiltList<int> get kids;
+  String get url;
+  int get score;
+  String get title;
+  BuiltList<int> get parts;
+  int get descendants;
 
   Article._();
   factory Article([void Function(ArticleBuilder) updates]) = _$Article;
 }
-
-// class Article {
-//   final String text;
-//   final String url;
-//   final String by;
-//   final int time;
-//   final int score;
-
-//   const Article({this.text, this.url, this.by, this.time, this.score});
-
-//   factory Article.fromJson(Map<String, dynamic> json) {
-//     if (json == null) return null;
-
-//     return Article(
-//         text: json['text'] ?? '[null]',
-//         url: json['url'],
-//         by: json['by'],
-//         time: json['time'],
-//         score: json['score']);
-//   }
-// }
 
 List<int> parseTopStories(String jsonStr) {
   return [];
