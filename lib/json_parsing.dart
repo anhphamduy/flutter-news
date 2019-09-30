7d2a4dc9a3606cc89020b9d8ba6e8084f2559573
Anh Pham <anh.pham@lodlaw.com>
2019-09-30 20:14:29 +1000

    Add built values

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,34 +1,46 @@
 import 'dart:convert' as json;
+import 'package:built_value/built_value.dart';
 
-class Article {
-  final String text;
-  final String url;
-  final String by;
-  final int time;
-  final int score;
+part 'json_parsing.g.dart';
 
-  const Article({this.text, this.url, this.by, this.time, this.score});
+abstract class Article implements Built<Article, ArticleBuilder> {
+  int get id;
 
-  factory Article.fromJson(Map<String, dynamic> json) {
-    if (json == null) return null;
-
-    return Article(
-        text: json['text'] ?? '[null]',
-        url: json['url'],
-        by: json['by'],
-        time: json['time'],
-        score: json['score']);
-  }
+  Article._();
+  factory Article([void Function(ArticleBuilder) updates]) = _$Article;
 }
 
+// class Article {
+//   final String text;
+//   final String url;
+//   final String by;
+//   final int time;
+//   final int score;
+
+//   const Article({this.text, this.url, this.by, this.time, this.score});
+
+//   factory Article.fromJson(Map<String, dynamic> json) {
+//     if (json == null) return null;
+
+//     return Article(
+//         text: json['text'] ?? '[null]',
+//         url: json['url'],
+//         by: json['by'],
+//         time: json['time'],
+//         score: json['score']);
+//   }
+// }
+
 List<int> parseTopStories(String jsonStr) {
-  final parsed = json.jsonDecode(jsonStr);
-  final listOfIds = List<int>.from(parsed);
-  return listOfIds;
+  return [];
+  // final parsed = json.jsonDecode(jsonStr);
+  // final listOfIds = List<int>.from(parsed);
+  // return listOfIds;
 }
 
 Article parseArticle(String jsonStr) {
-  final parsed = json.jsonDecode(jsonStr);
-  Article article = Article.fromJson(parsed);
-  return article;
+  return null;
+  // final parsed = json.jsonDecode(jsonStr);
+  // Article article = Article.fromJson(parsed);
+  // return article;
 }
